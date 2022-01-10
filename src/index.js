9bb702675bb2ddcab2aafb5bd50118f921d97288
Jennie Rozenberg <jennie.rozenberg@gmail.com>
2022-01-10 18:35:05 -0500

    Add Conversion to Celcius/Fahrenheit functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+function convertFToC(degrees) {
+  return Math.round((degrees-32) * 5/9)
+}
 
+function convertCToF(degrees) {
+  return Math.round((degrees * 9/5) + 32)
+}
 function formatDate(timestamp) {
   let date = new Date(timestamp * 1000);
   let hours = date.getHours();
@@ -52,7 +58,7 @@ function showCurrentTemperature(response) {
   cityDisplay.innerHTML = `${cityName}, ${country}`;
   currentTemperature = Math.round(currentTemperature);
   displayWeatherConditions.innerHTML = weatherConditions;
-  displayTemperature.innerHTML = `${currentTemperature}°F`;
+  displayTemperature.innerHTML = `${currentTemperature}°F (${convertFToC(currentTemperature)}°C)`;
   humidityElement.innerHTML = `${response.data.main.humidity}%`; 
   windElement.innerHTML = `${Math.round(response.data.wind.speed)} mph`; 
   weatherIcon.setAttribute ("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
